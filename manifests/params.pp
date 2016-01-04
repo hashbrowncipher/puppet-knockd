458d0e6a935c39cacbfb449b247b630b3b206158
Josh Snyder <josnyder@yelp.com>
2016-01-04 14:54:34 -0800

    Actually support debian maybe

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,35 +5,42 @@
 # Copyright 2015 Alessio Cassibba (X-Drum), unless otherwise noted.
 #
 class knockd::params {
-	$service_name = 'knockd'
-	$usesyslog = undef
-	$logfile = '/var/log/knockd.log'
-	$pidfile = '/var/run/knockd.pid'
-	$interface = undef
-	$sequence = undef
-	$open_sequence = undef
-	$close_sequence = undef
-	$one_time_sequences = undef
-	$seq_timeout = undef
-	$tcpflags = undef
-	$start_command = undef
-	$stop_command = undef
-	$cmd_timeout = undef
+  $service_name = 'knockd'
+  $usesyslog = undef
+  $logfile = '/var/log/knockd.log'
+  $pidfile = '/var/run/knockd.pid'
+  $interface = undef
+  $sequence = undef
+  $open_sequence = undef
+  $close_sequence = undef
+  $one_time_sequences = undef
+  $seq_timeout = undef
+  $tcpflags = undef
+  $start_command = undef
+  $stop_command = undef
+  $cmd_timeout = undef
   $command = undef
   $package_ensure = 'present'
 
-	case $::kernel {
-		FreeBSD: {
-			$config_file = '/usr/local/etc/knockd.conf'
-			$default_owner = 'root'
-			$default_group = 'wheel'
-			$package_name = 'knock'
-		}
-		Linux,default: {
-			$config_file = '/etc/knockd.conf'
-			$default_owner = 'root'
-			$default_group = 'root'
-			$package_name = 'knock-server'
-		}
-	}
+  case $::kernel {
+    FreeBSD: {
+      $config_file = '/usr/local/etc/knockd.conf'
+      $default_owner = 'root'
+      $default_group = 'wheel'
+      $package_name = 'knock'
+    }
+    Linux,default: {
+      case $::osfamily {
+        'Debian': {
+          $package_name = 'knockd'
+        }
+        default: {
+          $package_name = 'knock-server'
+        }
+      }
+      $config_file = '/etc/knockd.conf'
+      $default_owner = 'root'
+      $default_group = 'root'
+    }
+  }
 }
